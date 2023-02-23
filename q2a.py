cc7e70a62b58eacd8a82031b4ca84c31424c613f
anh-nguyen-katalon <anh.hnnguyen@katalon.com>
2023-02-23 14:17:58 +0700

    convert fastq to fasta

---
+++ q2a.py
@@ -0,0 +1,21 @@
+def main():
+    input_file_name = input("FASTQ file (without file extension): ") #example input: SRR10003462_1
+
+    output_file_name = f'{input_file_name}.fasta'
+    output_file = open(output_file_name, 'w+')
+
+    try:
+        with open (f'{input_file_name}.fastq', 'r') as f:
+            while True:
+                marker = f.readline() # read marker (1st line)
+                if marker:
+                    output_file.write(f'>{marker.split()[0]}\n')
+                output_file.write(f'{f.readline()}') # read sequence (2nd line)
+                f.readline()
+                f.readline()
+    except Exception as e:
+        print (e)
+    finally:
+        output_file.close()
+   
+main()
